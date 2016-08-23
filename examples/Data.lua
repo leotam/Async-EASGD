846a40f7ddc1ae0c782ca606f011a9a55fb8a512
leotam <leot@leot-mlt.local>
2016-08-23 10:08:01 -0700

    mnist

--- examples/Data.lua
+++ examples/Data.lua
@@ -7,22 +7,22 @@ local function Data(partitionNum, partitions, batchSize, cuda)
   local lfs = require 'lfs'
   dirPrefix = string.match(lfs.currentdir(),"/%a+/%a+/")
 
-  local trainingDataset = Dataset(dirPrefix .. 'Datasets/Cifar10/cifar10-train-twitter.t7', {
+  local trainingDataset = Dataset(dirPrefix .. 'Datasets/mnist/train.t7', {
      -- Partition dataset so each node sees a subset:
      partition = partitionNum,
      partitions = partitions
   })
 
-  local testDataset = Dataset(dirPrefix .. 'Datasets/Cifar10/cifar10-test-twitter.t7', {
+  local testDataset = Dataset(dirPrefix .. 'Datasets/mnist/test.t7', {
      -- Partition dataset so each node sees a subset:
      partition = partitionNum,
      partitions = partitions
   })
 
   local getTrainingBatch, numTrainingBatches = trainingDataset.sampledBatcher({
-     samplerKind = 'label-uniform',
+     samplerKind = 'permutation',
      batchSize = batchSize,
-     inputDims = { 3, 32, 32 },
+     inputDims = { 1024 },
      verbose = true,
      cuda = cuda,
      processor = function(res, processorOpt, input)
@@ -34,16 +34,16 @@ local function Data(partitionNum, partitions, batchSize, cuda)
         -- Decompress the PNG bytes into a Tensor
         -- local pixels = image.decompressPNG(bytes)
         -- Copy the pixels tensor into the mini-batch
-        input:copy(res)
+        input:copy(res:view(1, 1024))
         return true
      end,
   })
 
 
   local getTestBatch, numTestBatches = testDataset.sampledBatcher({
-     samplerKind = 'label-uniform',
+     samplerKind = 'permutation',
      batchSize = batchSize,
-     inputDims = { 3, 32, 32 },
+     inputDims = { 1024 },
      verbose = true,
      cuda = cuda,
      processor = function(res, processorOpt, input)
@@ -55,15 +55,14 @@ local function Data(partitionNum, partitions, batchSize, cuda)
         -- Decompress the PNG bytes into a Tensor
         -- local pixels = image.decompressPNG(bytes)
         -- Copy the pixels tensor into the mini-batch
-        input:copy(res)
+        input:copy(res:view(1, 1024))
         return true
      end,
   })
 
-  local classes = {
-     'airplane', 'automobile', 'bird', 'cat',
-     'deer', 'dog', 'frog', 'horse', 'ship', 'truck'
-  }
+  -- Load in MNIST
+  local classes = {'0','1','2','3','4','5','6','7','8','9'}
+  local confusionMatrix = optim.ConfusionMatrix(classes)
 
   return {
     getTrainingBatch = getTrainingBatch,
